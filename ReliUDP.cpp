83075dfb77661345b7fac96b059ba79f48b07b66
paroid <chenke625@gmail.com>
2013-07-25 16:59:52 +0800

    resetCom() bug fixed

--- ReliUDP.cpp
+++ ReliUDP.cpp
@@ -117,8 +117,6 @@ void ReliUDP::startCom() {
 }
 
 void ReliUDP::resetCom(SOCKADDR_IN addr) {
-    if(!ST.haveIPort(getIPort(addr))) //not my client no need to reset
-        return;
     resetWaitFlag = true;
     int waitTime = 8;
     do {
